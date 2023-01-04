94445e107e235febf76724b97f02eb53032fccde
dunghoang22102003 <104152321+dunghoang22102003@users.noreply.github.com>
2023-01-04 14:53:06 +0700

    update

---
+++ assets/js/home.js
@@ -0,0 +1,20 @@
+function click(number) {
+    if (number === 1) {
+        location.href = "home.html";
+    }
+    if (number === 2) {
+        location.href = "home.html";
+    }
+    if (number === 3) {
+        location.href = "home.html";
+    }
+    if (number === 4) {
+        location.href = "home.html";
+    }
+    if (number === 5) {
+        location.href = "home.html";
+    }
+    if (number === 6) {
+        location.href = "home.html";
+    }
+}
